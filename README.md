c25d7082d1b2f37c644325ae2030d4bc56e6b1ac
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-11 22:13:31 +0530

    Added: Given 3 Integers find the maximum

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Generic_Find_Maximum
+
+**UC1-FindMaximumInteger**
+
+**_Description_** : Given 3 Integers find the maximum
+- Ensure to test code with the Test Case.
+- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
+- Use Integer Object and compareTo UC 1 method to test the maximum number
--- bridgelabz/GenericsFindMax.java
+++ bridgelabz/GenericsFindMax.java
@@ -1,1 +1,33 @@
-//Welcome to Find Maximum Using Generics
+package bridgelabz;
+
+public class GenericsFindMax <T extends Comparable<T>> {
+
+    /**
+     * Determines the largest of three numbers.
+     * @param x
+     * @param y
+     * @param z
+     * @param <T>
+     * @return
+     */
+    public <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
+        T max = x;
+
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+
+        return max;
+    }
+
+    /**
+     * Main method or Starting point of this program
+     * @param args
+     */
+    public static void main(String[] args) {
+        GenericsFindMax genericsFindMax = new GenericsFindMax();
+        genericsFindMax.findMaximum(6,7,5);
+    }
+}
